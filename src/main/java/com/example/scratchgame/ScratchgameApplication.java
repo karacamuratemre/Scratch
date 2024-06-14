57b85ff2f8a19fda14607bbb811ea2d5e0558bb2
karacamuratemre <karacamuratemre@gmail.com>
2024-06-14 15:37:07 +0300

    changes

---
+++ src/main/java/com/example/scratchgame/ScratchgameApplication.java
@@ -0,0 +1,13 @@
+package com.example.scratchgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScratchgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScratchgameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scratchgame/ScratchgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scratchgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScratchgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
